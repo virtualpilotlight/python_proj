e2f65c14d25c482b0e83b679161a5bc02871c4a7
Omega X Rae <63996271+virtualpilotlight@users.noreply.github.com>
2022-02-04 22:00:09 -0800

    Add files via upload

---
+++ 2nd_largest.py
@@ -0,0 +1,3 @@
+list_of_num =  [1, 20, 300, 22, 9, 0]
+list_of_num.sort(reverse = True)
+print(list_of_num[1])
---
+++ print_loop.py
@@ -0,0 +1,3 @@
+list_of_num =  [1, 20, 300, 22, 9, 0]
+for x in list_of_num:
+	print(x)
