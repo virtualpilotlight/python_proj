9b0da148696ff59e23bb035b85953fda5463cbde
Omega Rae <omega.x.rae@gmail.com>
2022-02-10 18:18:31 -0800

    updated time converssion program

--- days_to_hours.py
+++ days_to_hours.py
@@ -6,26 +6,23 @@ total = 24
 units = "hours"
 # a function is assigned with def name_of_func(params):
 def days_to_units(num_of_days):
+	# return returns the value, for concat use f
+	return f"{num_of_days} days are {num_of_days * total} {units}."
 
-	condition_check = num_of_days > 0
-	print(type(condition_check))
-	#conditional statments like if get a :
-	if num_of_days > 0:
-		# return returns the value, for concat use f
-		return f"{num_of_days} days are {num_of_days * total} {units}."
-	elif num_of_days  == 0:
-		return "zero doesn't makes sense. positives only."
-
+def validate_and_execute():
+	if user_input.isdigit():
+		# int() converts the str to an int,
+		user_num = int(user_input)
+		if user_num > 0:	
+			# this runs the function
+			calculated_value = days_to_units(user_num)
+			# this prints the results of the finished function
+			print(calculated_value)
+		elif user_num == 0:
+			print("zero doesn't makes sense. positives only.")
+	else:
+		print("whole positive numbers only.")	
 
 # user input is storred as a string
 user_input = input("pick a number:\n")
-
-if user_input.isdigit():
-	# int() converts the str to an int,
-	user_num = int(user_input)
-	# this runs the function
-	calculated_value = days_to_units(user_num)
-	# this prints the results of the finished function
-	print(calculated_value)
-else:
-	print("whole positive numbers only.")
+validate_and_execute()
