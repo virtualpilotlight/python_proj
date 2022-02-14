8ea5f9ae8fccaadfb5e517811f6acd5bac70b976
Omega Rae <omega.x.rae@gmail.com>
2022-02-13 20:44:18 -0800

    soup parses for links on page to a set

--- aml_spider.py
+++ aml_spider.py
@@ -3,9 +3,22 @@
 from bs4 import BeautifulSoup
 import requests
 url = "http://allmylove.org/audio/"
+
+
 req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
 print(soup)
+
+urls = {url}
+
 for link in soup.find_all('a'):
-	print (url + link.get('href'))
+	new_url = url + link.get('href')
+	print (new_url)
+	print(type(new_url))
+	urls.add(new_url)
+	print(urls)
+	print(type(urls))
+
+
+
 
