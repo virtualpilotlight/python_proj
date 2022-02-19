af681f4de0cc5ae719ec25b70be33251233ab90b
Omega Rae <omega.x.rae@gmail.com>
2022-02-18 21:08:05 -0800

    inventory .xlxs completed

---
+++ inventory_checker.py
@@ -0,0 +1,42 @@
+import openpyxl
+
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
+
+products_per_supplier = {}
+total_value_per_supplier = {}
+products_under_10_inv = {}
+
+print(product_list.max_row)
+
+for prodcut_row in range(2, product_list.max_row + 1):
+	supplier_name = product_list.cell(prodcut_row, 4).value
+	inventory = product_list.cell(prodcut_row, 2).value
+	price = product_list.cell(prodcut_row, 3).value
+	product_num = product_list.cell(prodcut_row, 1).value
+	inventory_price = product_list.cell(prodcut_row, 5)
+	# number of produts per supplier
+	if supplier_name in products_per_supplier:
+		current_num_products = products_per_supplier.get(supplier_name)
+		products_per_supplier[supplier_name] = current_num_products + 1
+	else:
+		print("adding a new supplier")
+		products_per_supplier[supplier_name] = 1
+
+
+	if supplier_name in total_value_per_supplier:
+		current_total_value = total_value_per_supplier.get(supplier_name)
+		total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+	else:
+		total_value_per_supplier[supplier_name] = inventory * price
+
+	if inventory < 10:
+		products_under_10_inv[int(product_num)] = int(inventory)
+
+	inventory_price.value = inventory * price
+
+print(products_under_10_inv)		
+print(products_per_supplier)
+print(total_value_per_supplier)
+
+inv_file.save("inventory_with_total_value.xlsx")
